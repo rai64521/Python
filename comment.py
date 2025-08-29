f1592d14311aed19fd05bf0b67b39d32d6a92c30
rai64521 <raisumant99@gmail.com>
2025-08-29 14:10:04 +0530

    comment and variables in python

---
+++ comment.py
@@ -0,0 +1,6 @@
+# comment in python
+
+#This is a comment
+
+
+# print("comment")
---
+++ variables.py
@@ -0,0 +1,39 @@
+# variables
+# variables is a container to store data  
+
+name = "Sumant Rai"
+
+print(name)
+
+# In python no variables decalaration
+
+# python is Dynamic Typing programming language
+name = "Shubhangi Mishra"
+
+print(name)
+
+name = 12345
+
+print(name)
+
+# one vairales store mutliples datatypes that is dynamic binding
+
+
+
+a = 2; b = 4; c = 6
+
+print(a)
+print(b)
+print(c)
+
+a, b, c, = 2, 4, 6
+
+print(a)
+print(b)
+print(c)
+
+a = b = c = 10
+print(a)
+print(b)
+print(c)
+
