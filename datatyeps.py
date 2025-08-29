d0ca249d668772f12be75d495af1b19ce794b858
rai64521 <raisumant99@gmail.com>
2025-08-29 13:55:09 +0530

    datatypes in python

---
+++ datatyeps.py
@@ -0,0 +1,37 @@
+# integer
+print(4)
+print(1e309)
+
+#float
+print(4.3)
+print(1.7e308)
+
+#boolean
+print(True)
+print(False)
+
+#complex
+print(5 + 3j)
+
+#string
+
+print('Mumbai')
+print("Mumbai")
+print("""Mumbai""")
+
+
+#List
+
+print([1, 2, 3, 4, 5, 6])
+
+#Tuples
+
+print((1, 2, 3, 4, 5, 6))
+
+#sets
+
+print({1, 2, 3, 4, 5, 6})
+
+#dictonary
+
+print({"name": "Sumant Rai", "age" : 21})
