0f74b3aacbaa63bd397a8fa9906b98a2b4080073
rai64521 <raisumant99@gmail.com>
2025-08-29 13:40:27 +0530

    python print function

---
+++ test.py
@@ -0,0 +1,7 @@
+print("Hello world in python")
+
+print("india","nepal","shrilanka","pakistan","butan")
+
+
+print("india", "nepal", "shrilanka", "pakistan", "butan", sep='/')
+print("india", "nepal", "shrilanka", "pakistan", "butan", sep='-')
